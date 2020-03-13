a79eec63b4e8c0e0d81b39a1e330b2b3ae17afc3
david.tian <david.tian@live.com>
2020-03-14 02:11:42 +0800

    add WordCount Application in scala-spark and java-spark module.

--- java-spark/src/main/java/com/dvtn/core/WordCount.java
+++ java-spark/src/main/java/com/dvtn/core/WordCount.java
@@ -1,7 +1,91 @@
 package com.dvtn.core;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class WordCount {
     public static void main(String[] args) {
+        /**
+         * SparkConf:
+         * 1.conf可以设置spark的运行模式
+         * 2.可以设置spark在webui中显示的application名称。
+         * 3.设置当前Spark Application运行的资源(内存与core)。(通过set方法)
+         *      比如电脑是4核8线程：说明每个核是双线程。spark中的cores与线程数相对应。说明这台电脑可以提供spark应用8个cores。
+         *
+         * spark运行模式
+         *  1.local -- 在eclipse,IDEA中开发spark程序要用local模式，本地模式，多用于测试模式。
+         *  2.standalone: spark自带的资源调度框架，支持分布式搭建，spark任务可以依赖standalone调度资源。
+         *  3.yarn: hadoop生态圈中资源调度框架，spark也可以yarn调度资源。
+         *  4.mesos: 资源调度框架。
+         */
+        SparkConf conf = new SparkConf();
+        conf.setMaster("local");
+        conf.setAppName("JavaSparkWordCount");
+        //conf.set("spark.submit.deployMode","client" );
+
+        /**
+         * SparkContext:
+         * 它是通往集群的唯一通道。
+         */
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        /**
+         * sc.textFile 读取文件
+         */
+        JavaRDD<String> lines = jsc.textFile("./java-spark/data/words");
+
+        /**
+         * flatMap：进一条数据出多条数据
+         */
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+
+            @Override
+            public Iterator<String> call(String line) throws Exception {
+                return Arrays.asList(line.split(" ")).iterator();
+            }
+        });
+
+        /**
+         * 在java中如果想让某个RDD转换成K,V格式使用xxxToPair
+         */
+        JavaPairRDD<String, Integer> pairWords = words.mapToPair(new PairFunction<String, String, Integer>() {
+
+            @Override
+            public Tuple2<String, Integer> call(String word) throws Exception {
+                return new Tuple2<String, Integer>(word, 1);
+            }
+        });
+
+        /**
+         * reduceByKey
+         * 1.先将相同的key分组
+         * 2.对每一组的key对应的value去按照自定义的逻辑进行处理
+         */
+        JavaPairRDD<String, Integer> reduceWords = pairWords.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+
+        reduceWords.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+            @Override
+            public void call(Tuple2<String, Integer> tuple) throws Exception {
+                System.out.println(tuple);
+            }
+        });
+
+        jsc.stop();
 
     }
 }
--- scala-spark/src/main/scala/com/dvtn/spark/core/WordCount.scala
+++ scala-spark/src/main/scala/com/dvtn/spark/core/WordCount.scala
@@ -5,39 +5,44 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setMaster("local").setAppName("wordcount")
-
-    val sc = new SparkContext(conf)
-
-    val lines = sc.textFile("./scala-spark/data/words").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).foreach(println)
-
-    sc.stop()
-
-
-//    val conf = new SparkConf()
-//    conf.setMaster("local").setAppName("wordcount")
+//    val conf = new SparkConf().setMaster("local").setAppName("wordcount")
 //
 //    val sc = new SparkContext(conf)
 //
-//    val lines: RDD[String] = sc.textFile("./scala-spark/data/words")
-//
-//    val words: RDD[String] = lines.flatMap(line => {
-//      line.split(" ")
-//    })
-//
-//    val pairWords: RDD[(String, Int)] = words.map(word => {
-//      new Tuple2(word, 1)
-//    })
-//
-//    val reducedWords: RDD[(String, Int)] = pairWords.reduceByKey((v1: Int, v2: Int) => {
-//      v1 + v2
-//    })
-//
-//    reducedWords.foreach(tuple => {
-//      println(tuple)
-//    })
+//    val lines = sc.textFile("./scala-spark/data/words").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).foreach(println)
 //
 //    sc.stop()
+
+
+    val conf = new SparkConf()
+    conf.setMaster("local").setAppName("wordcount")
+
+    val sc = new SparkContext(conf)
+
+    val lines: RDD[String] = sc.textFile("./scala-spark/data/words")
+
+    val words: RDD[String] = lines.flatMap(line => {
+      line.split(" ")
+    })
+
+    val pairWords: RDD[(String, Int)] = words.map(word => {
+      new Tuple2(word, 1)
+    })
+
+    val reducedWords: RDD[(String, Int)] = pairWords.reduceByKey((v1: Int, v2: Int) => {
+      v1 + v2
+    })
+
+//    val result = reducedWords.sortBy(tuple=>{
+//      tuple._2
+//    },false)
+    val result = reducedWords.map(tuple=>{tuple.swap}).sortByKey(false).map(tuple=>{tuple.swap})
+
+    result.foreach(tuple => {
+      println(tuple)
+    })
+
+    sc.stop()
 
   }
 }
