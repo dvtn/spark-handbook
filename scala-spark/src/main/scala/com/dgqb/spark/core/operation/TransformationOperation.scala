d5e427025b3dd4f328e1d177bea81632c546cc3c
david.tian <david.tian@live.com>
2020-03-19 10:12:52 +0800

    算子用法更新

--- scala-spark/src/main/scala/com/dgqb/spark/core/operation/TransformationOperation.scala
+++ scala-spark/src/main/scala/com/dgqb/spark/core/operation/TransformationOperation.scala
@@ -2,6 +2,8 @@ package com.dgqb.spark.core.operation
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+import scala.collection.mutable.ListBuffer
+
 object TransformationOperation {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local").setAppName("TransformationOperationTest")
@@ -11,26 +13,104 @@ object TransformationOperation {
     /**
      * groupByKey
      */
+    val rdd = sc.parallelize(Array(
+      ("金庸","飞狐外传"),
+      ("金庸","雪山飞弧"),
+      ("金庸","连成诀"),
+      ("金庸","天龙八部"),
+      ("金庸","白马啸西风"),
+      ("金庸","鹿鼎记"),
+      ("金庸","笑傲江湖"),
+      ("金庸","书剑恩仇录"),
+      ("金庸","神雕侠女"),
+      ("金庸","侠客行"),
+      ("金庸","倚天屠龙记"),
+      ("金庸","碧血剑"),
+      ("金庸","鸳鸯刀"),
+      ("古龙","小李飞刀"),
+      ("古龙","楚留香传奇"),
+      ("古龙","陆小凤传奇"),
+      ("古龙","浣花洗剑录"),
+      ("古龙","武林外史"),
+      ("古龙","萧十一郎"),
+      ("古龙","绝代双骄"),
+      ("古龙","流星·蝴蝶·剑")
 
-    /**
-     * zipWithIndex
-     */
+    ), 3);
 
-    /**
-     * zip
-     */
+    rdd.groupByKey().foreach(println)
 
-    /**
-     * coalesce
-     */
 
-    /**
-     * repartition
-     */
+//    /**
+//     * zipWithIndex
+//     */
+//    val rdd1 = sc.parallelize(Array(
+//      "飞狐外传","雪山飞弧","连成诀","天龙八部",
+//      "射雕英雄传","白马啸西风","鹿鼎记","笑傲江湖",
+//      "书剑恩仇录","神雕侠女","侠客行","倚天屠龙记"
+//    ),3);
+//
+//    rdd1.zipWithIndex().foreach(println)
 
-    /**
-     * mapPartitionWithIndex
-     */
+//    /**
+//     * zip
+//     * 把两个rdd拼接成KV格式的rdd
+//     */
+//    val rdd1 = sc.parallelize(Array(
+//      "飞狐外传","雪山飞弧","连成诀","天龙八部",
+//      "射雕英雄传","白马啸西风","鹿鼎记","笑傲江湖",
+//      "书剑恩仇录","神雕侠女","侠客行","倚天屠龙记"
+//    ),3);
+//    val rdd2 = sc.parallelize(Array(
+//      "胡斐","苗人凤","狄云","段誉",
+//      "郭靖","上官虹","韦小宝","令狐冲",
+//      "陈家洛","杨过","石中玉","张君宝"
+//    ),3);
+//    rdd1.zip(rdd2).foreach(println)
+
+
+//
+//    /**
+//     * mapPartitionWithIndex
+//     */
+//    val rdd = sc.parallelize(Array(
+//      "飞狐外传","雪山飞弧","连成诀","天龙八部",
+//      "射雕英雄传","白马啸西风","鹿鼎记","笑傲江湖",
+//      "书剑恩仇录","神雕侠女","侠客行","倚天屠龙记"
+//    ),3);
+//    val rdd1 = rdd.mapPartitionsWithIndex((index, iter)=>{
+//      val list = new ListBuffer[String]()
+//      while(iter.hasNext){
+//        list.append("rdd partition index = "+index+", value = "+ iter.next())
+//      }
+//      list.iterator
+//    },true)
+//
+//    rdd1.foreach(println)
+//
+//    /**
+//     * repartition
+//     */
+//    rdd1.repartition(4).mapPartitionsWithIndex((index,iter)=>{
+//      val list = new ListBuffer[String]()
+//      while(iter.hasNext){
+//        list.append("repartition partition index = "+index+", value = "+ iter.next())
+//      }
+//      list.iterator
+//    },true).foreach(println)
+//
+//    /**
+//     * coalesce
+//     */
+//    rdd1.coalesce(4,true).mapPartitionsWithIndex((index, iter)=>{
+//      val list = new ListBuffer[String]()
+//      while(iter.hasNext){
+//        list.append("coalesce partition index = "+index+", value = "+ iter.next())
+//      }
+//      list.iterator
+//    },true).foreach(println)
+
+
 
     //    /**
 //     * mapPartitions 一个分区一个分区地处理数据
